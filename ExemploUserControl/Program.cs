390825d437e213d2baad105435a938f70b710d5a
Aluno <aluno@band.net>
2016-03-23 19:12:49 -0300

    Criando user controll - Acao

--- ExemploUserControl/Program.cs
+++ ExemploUserControl/Program.cs
@@ -16,7 +16,7 @@ namespace ExemploUserControl
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new Cozinha());
 		}
 	}
 }
