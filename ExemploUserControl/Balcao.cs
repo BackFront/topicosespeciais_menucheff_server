5a312b88c1c6ef4c0cc942ba554e8832eff6853a
Douglas <alves.douglaz@gmail.com>
2016-10-07 10:26:11 -0300

    Sync

---
+++ ExemploUserControl/Balcao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploUserControl
+{
+	public partial class Balcao : Form
+	{
+		public Balcao()
+		{
+			InitializeComponent();
+		}
+
+		public void recebeItem(Itens nomeItem)
+		{
+			flowLayoutPanel1.Controls.Add(nomeItem);	
+        }
+
+
+
+
+
+	}
+}
